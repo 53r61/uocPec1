7df2770d5cbf7b24eea0efc9e367a2a88f21e989
squella <squella100@gmail.com>
2018-11-09 23:34:54 +0100

    sin terminar modografico

--- ejercicio4.sh
+++ ejercicio4.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
- 
+#filtramos en el archivo passwd que es donde se guardan los usuarios, lo hacemos con la columna 3 ya que ahí es donde estan el nombre de los usuarios, ponemos 3==0 para el root y 3==500 para el resto, ya que es apartir de UDI 500 dondo los identificamos 
+#guardamos todo en nuestra variable usarios
 usuarios=$(awk -F: '{if ($3==0 || $3>500) print $1}' /etc/passwd)
-VAR=date
+#recorro la variable con un for y voy creando los archivos con los nombres de los usuarios a la vez que creo la ruta y meto la informacion del usu que le han dado al sistema
 for i in ${usuarios}
 do
 	mkdir -p /root/informes/ 
-	ps -u $i >> /root/informes/$i&&$VAR
-	cat /root/informes/$i&&$VAR
-	echo $VAR
+	ps -u $i >> /root/informes/$i
+
 done
 
